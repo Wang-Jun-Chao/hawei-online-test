eb2ea1a152cef8623e94f6fff6bfe06147f00d45
shining-glory <shining-glory@qq.com>
2015-12-28 18:32:09 +0800

    commit

--- 066-求解立方根/src/Main.java
+++ 066-求解立方根/src/Main.java
@@ -7,30 +7,24 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        //Scanner scanner = new Scanner(System.in);
-        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
+        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(Main.class.getClassLoader().getResourceAsStream("data.txt"));
         while (scanner.hasNext()) {
             double d = scanner.nextDouble();
-            System.out.printf("%g\n", getCubeRoot(d, d));
+            System.out.printf("%.1f", getCubeRoot(d));
         }
 
         scanner.close();
     }
 
-    private static double getCubeRoot(double x, double a) {
+    private static double getCubeRoot(double x) {
 
-//        double x1 = 2.0 / 3.0 * x + a / (3.0 * x * x);
-//        if (x - x1 > -0.000000001 && x - x1 < 0.0000000001)
-//            return x1;
-//        else
-//            return getCubeRoot(x1, a);
         double x0;
-        double x1 = a;
+        double x1 = x;
         do {
             x0 = x1;
-            x1 = 2.0 / 3.0 * x0 + a / (3.0 * x0 * x0);
-
-        } while (Math.abs(x1 - x0) < 0.000001);
+            x1 = 2.0 / 3.0 * x0 + x / 3.0 / (x0 * x0);
+        } while (Math.abs(x1 - x0) > 0.000001);
 
         return x1;
     }
