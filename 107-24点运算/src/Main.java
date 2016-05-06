e7ec0a4ec1b3a3f9082ce7a3f75b3cbb724ea4c9
王俊超 <shining-glory@qq.com>
2016-05-06 10:17:46 +0800

    commit

--- 107-24点运算/src/Main.java
+++ 107-24点运算/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.LinkedList;
 import java.util.Scanner;
 
 /**
@@ -40,6 +39,7 @@ public class Main {
             return "ERROR";
         }
 
+        // 将牌转换成数字
         int[] ints = new int[parts.length];
         for (int i = 0; i < ints.length; i++) {
             ints[i] = convert(parts[i]);
@@ -66,29 +66,43 @@ public class Main {
      */
     private static boolean point24(int[] opd, String[] r) {
 
-        char[] opt = {'+', '+', '+'};
+        char[] opt = new char[opd.length - 1];
         char[] all = {'+', '-', '*', '/'};
         return point24(opd, opt, all, 0, r);
     }
 
     /**
-     * @param opd
-     * @param opt
-     * @param all
-     * @param n
-     * @param r
-     * @return
+     * @param opd 操作数
+     * @param opt 用于计算的操作符
+     * @param all 可以使用的全部操作符
+     * @param n   当前处理的数
+     * @param r   用于保存表达式结果
+     * @return true，计算的值为24，false，计算的值不是24
      */
     private static boolean point24(int[] opd, char[] opt, char[] all, int n, String[] r) {
 
         // 处理最后一个数字
         if (n == opd.length - 1) {
-
+            if (calculate(opd, opt, r)) {
+                return true;
+            }
         } else {
             for (int i = n; i < opd.length; i++) {
+                int temp = opd[n];
+                opd[n] = opd[i];
+                opd[i] = temp;
                 for (int j = 0; j < all.length; j++) {
-//                    opd[]
+                    opt[n] = all[j];
+                    boolean find = point24(opd, opt, all, n + 1, r);
+                    if (find) {
+                        return true;
+                    }
                 }
+
+                //  现场还原
+                temp = opd[n];
+                opd[n] = opd[i];
+                opd[i] = temp;
             }
         }
 
@@ -97,143 +111,30 @@ public class Main {
     }
 
     /**
-     * 4个[1, 10]的能否通过加减乘除，得到数字为24
+     * 计算值
      *
-     * @param arr    能够使用的操作数、操作符的数组
-     * @param used   已经使用的操作数、操作符标记数组
-     * @param numCnt 操作数的个数
-     * @param optCnt 操作符的个数
-     * @param list   求得的逆波兰式
-     * @param result 用于保存结果
-     * @param rst    保存中间结果，有满足24的就停止计算
+     * @param opd 操作数
+     * @param opt 操作符
+     * @param r   保存结果表达式
+     * @return true，结果为24，false结果不是24
      */
-    private static void to24(int[] arr, boolean[] used, int numCnt, int optCnt,
-                             LinkedList<Integer> list, boolean[] rst, String[] result) {
+    private static boolean calculate(int[] opd, char[] opt, String[] r) {
 
-
-        // 如果已经找到答案就不进行操作了
-        if (rst[0]) {
-            return;
-        }
-        // 已经完成了，计算结果
-        if (numCnt == 4 && optCnt == 4) {
-            caluclate(list, rst, result);
-        }
-        // 还要构造逆波兰式
-        else if (numCnt > optCnt) {
-
-            for (int i = 0; i < arr.length; i++) {
-                // 如果arr[i]还没有被使用过，或者arr[i]是运算符
-                if (!used[i] || arr[i] < 0 || arr[i] > 10) {
-                    // 如果是数字
-                    if (arr[i] >= 0 && arr[i] <= 10) {
-                        list.add(arr[i]);
-                        numCnt++;
-                        used[i] = true;
-
-                        to24(arr, used, numCnt, optCnt, list, rst, result);
-
-                        // 找到了一个答案就返回
-                        if (rst[0]) {
-                            return;
-                        }
-
-                        list.removeLast();
-                        numCnt--;
-                        used[i] = false;
-
-                    }
-                    // 如果是操作符，则放入arr[i]之前，操作数必须比操作符多两个
-                    else if (numCnt + 1 > optCnt) {
-                        list.add(arr[i]);
-                        optCnt++;
-                        used[i] = true;
-
-                        to24(arr, used, numCnt, optCnt, list, rst, result);
-
-                        // 找到了一个答案就返回
-                        if (rst[0]) {
-                            return;
-                        }
-                        list.removeLast();
-                        optCnt--;
-                        used[i] = false;
-                    }
-                }
-            }
-
+        double v = opd[0];
+        for (int i = 0; i < opt.length; i++) {
+            v = calculate(v, opd[i + 1], opt[i]);
         }
 
+        boolean eq = Math.abs(v - 24) < 0.0000001;
 
-    }
-
-    /**
-     * 计算逆波兰式的值
-     *
-     * @param list   逆波兰式
-     * @param rst    用于保存计算结果
-     * @param result 用于保存结果
-     */
-    private static void caluclate(LinkedList<Integer> list, boolean[] rst, String[] result) {
-//        LinkedList<Double> stack = new LinkedList<>();
-
-        System.out.println(list);
-        int i = 0;
-        double[] opd = new double[4];
-        int j = 0;
-        int[] opt = new int[3];
-
-
-        for (int v : list) {
-
-            // 如果是数字
-            if (v >= 0 && v < 14) {
-                opd[i] = v;
-                i++;
-            }
-            // 如是操作符
-            else {
-                opt[j] = v;
-                j++;
-
+        if (eq) {
+            r[0] = convert(opd[0]);
+            for (int i = 0; i < opt.length; i++) {
+                r[0] = r[0] + opt[i] + convert(opd[i + 1]);
             }
         }
 
-        double v = calculate(opd[0], opd[1], opt[0]);
-        v = calculate(v, opd[2], opt[1]);
-        v = calculate(v, opd[3], opt[2]);
-
-
-        rst[0] = v == 24.0;
-
-        if (rst[0]) {
-
-            result[0] = "" + opd[0] + opt[0] + opd[1] + opt[1] + opd[2] + opt[2] + opd[3];
-
-//            Deque<String> s = new LinkedList<>();
-//
-//            for (int i : list) {
-//                // 如果是数字
-//                if (i > 0 && i < 14) {
-//                    // 将数字转换成牌，再处理
-//                    s.addLast(convert(i));
-//                    System.out.print(convert(i));
-//                }
-//                // 如果是运算符
-//                else {
-//                    String y = s.removeLast();
-//                    String x = s.removeLast();
-//                    s.addLast(x + ((char) i) + y);
-//                    System.out.print((char) i);
-//                }
-//            }
-//
-//            System.out.println();
-//
-//            result[0] = s.removeLast();
-
-
-        }
+        return eq;
     }
 
     /**
@@ -302,6 +203,14 @@ public class Main {
         return "0";
     }
 
+    /**
+     * 表达式求值
+     *
+     * @param a   操作数一
+     * @param b   操作数二
+     * @param opt 操作符
+     * @return 运算结果
+     */
     private static double calculate(double a, double b, int opt) {
         switch (opt) {
             case '+':
