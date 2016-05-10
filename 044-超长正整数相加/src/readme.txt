3bbce12464dfef18beed98f06a0db4f0f86094c3
王俊超 <shining-glory@qq.com>
2016-05-11 07:22:08 +0800

    commit

--- 044-超长正整数相加/src/readme.txt
+++ 044-超长正整数相加/src/readme.txt
@@ -4,17 +4,17 @@
     请设计一个算法完成两个超长正整数的加法。
 
     接口说明
-    /*
-    请设计一个算法完成两个超长正整数的加法。
-    输入参数：
-    String addend：加数
-    String augend：被加数
-    返回值：加法结果
-    */
+    /**
+     * 请设计一个算法完成两个超长正整数的加法。
+     * 输入参数：
+     * String addend：加数
+     * String augend：被加数
+     * 返回值：加法结果
+     */
     public String addLongInteger(String addend, String augend) {
         /*在这里实现功能*/
 
-     return null;
+        return null;
     }
 输入描述:
     输入两个字符串数字
--- 045-计算日期到天数转换/src/Main.java
+++ 045-计算日期到天数转换/src/Main.java
@@ -21,15 +21,20 @@ public class Main {
     }
 
     private static int calculate(int year, int month, int day) {
+
         int[] dayOfMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        // 如果是闰年
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            dayOfMonth[1] = 29;
+        }
+
+        if (month < 1 || month > 12 || day < 1 || day > dayOfMonth[month - 1]) {
+            return -1;
+        }
+
 
         for (int i = 0; i < month - 1; i++) {
             day += dayOfMonth[i];
-        }
-
-        // 闰年
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            day++;
         }
 
         return day;
