4a001e250cebb048405250086efece0a3ffbc27c
王俊超 <shining-glory@qq.com>
2016-05-16 06:35:17 +0800

    commit

--- 066-求解立方根/src/Main.java
+++ 066-求解立方根/src/Main.java
@@ -17,6 +17,16 @@ public class Main {
         scanner.close();
     }
 
+    /**
+     * 使用牛顿迭代法求立方根
+     * 已知利用牛顿迭代法求方程F（x）=0的解的公式为X[n+1] = X[n] - F(X[n])/F'(X[n]),
+     * 其中x[n]为第n次利用此公式求得值。
+     * 假如函数F(X) = X^m - a, 则根据牛顿迭代法第n+1次求方程F(x) = 0的解为X[n+1]，
+     * 且X[n+1] = (1-1/m)*X[n] +a/(n*X[n]^(m-1))  -  (X[n]*X[n]*X[n]+a)/3*X[n]*X[n]。
+     *
+     * @param x
+     * @return
+     */
     private static double getCubeRoot(double x) {
 
         double x0;
